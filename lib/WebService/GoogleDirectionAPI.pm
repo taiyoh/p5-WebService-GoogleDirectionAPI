4e648b52cfb51879ec1ab9aa686397707806525a
taiyoh <sun.basix@gmail.com>
2011-12-08 12:03:58 +0900

    initial commit

---
+++ lib/WebService/GoogleDirectionAPI.pm
@@ -0,0 +1,32 @@
+package WebService::GoogleDirectionAPI;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::GoogleDirectionAPI -
+
+=head1 SYNOPSIS
+
+  use WebService::GoogleDirectionAPI;
+
+=head1 DESCRIPTION
+
+WebService::GoogleDirectionAPI is
+
+=head1 AUTHOR
+
+Taiyoh Tanaka E<lt>sun.basix@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
